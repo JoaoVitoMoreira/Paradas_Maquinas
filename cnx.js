0fcdbafef6d923202c3fddd5223dd42f6ac73346
Neu-Bauer <ens-kauanneubauer@ugv.edu.br>
2024-08-19 22:08:30 -0300

    commi funciona

--- cnx.js
+++ cnx.js
@@ -3,7 +3,7 @@ const { Pool} = require ('pg')
 const pool = new Pool ({
     user: 'postgres',
     password: 'c3xgqVOq7l3e',
-    host: 'http://paradas-maquinas.cvee4a6wqmi3.us-east-1.rds.amazonaws.com',
+    host: 'paradas-maquinas.cvee4a6wqmi3.us-east-1.rds.amazonaws.com',
     port: 5432,
     database: 'projeto_paradas'
 })
